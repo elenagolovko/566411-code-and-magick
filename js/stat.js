a45e5173e677affa711352c3d5200122854eb7e2
elenagolovko <airdragon220@gmail.com>
2018-04-02 21:59:25 +0300

    commit

--- js/stat.js
+++ js/stat.js
@@ -31,29 +31,33 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var renderWinText = function (ctx) {
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура! Вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT * 2);
+}
+
+var renderGistogramm = function (ctx, names, times) {
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillStyle = names[i] === 'Вы'
+      ? 'rgba(255, 0, 0, 1)'
+      : 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
+
+    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 2 - (barHeight * times[i]) / maxTime, BAR_WIDTH, (barHeight * times[i]) / maxTime);
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT);
+    ctx.fillText(Math.floor(times[i]), CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 3 - (barHeight * times[i]) / maxTime);
+  }
+}
+
 window.renderStatistics = function (ctx, names, times) {
 
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
-
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура! Вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT * 2);
-
-  var maxTime = getMaxElement(times);
-
-  for (var i = 0; i < names.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      var saturation = Math.floor(Math.random() * 100);
-      ctx.fillStyle = 'hsl(240, ' + saturation + '%' + ', 50%)';
-    }
-    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 2 - (barHeight * times[i]) / maxTime, BAR_WIDTH, (barHeight * times[i]) / maxTime);
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT);
-    ctx.fillText(Math.floor(times[i]), CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 3 - (barHeight * times[i]) / maxTime);
-  }
+  renderWinText(ctx);
+  renderGistogramm(ctx, names, times);
 };
